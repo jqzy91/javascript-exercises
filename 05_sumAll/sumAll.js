144d0bc5635fd7b2f30854477c7929c2a83e86fa
jqzy91 <jamesquek91@live.com.sg>
2022-03-29 23:03:17 +0800

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (startNum, endNum) {
+  let totalNum = 1;
+  if (
+    startNum < 0 ||
+    typeof startNum === "string" ||
+    typeof startNum === "object" ||
+    endNum < 0 ||
+    typeof endNum === "string" ||
+    typeof endNum === "object"
+  ) {
+    return "ERROR";
+  } else if (startNum < endNum) {
+    for (let i = startNum; i < endNum; i++) {
+      addNum = i + startNum;
+      totalNum += addNum;
+    }
+  } else if (startNum > endNum) {
+    for (let o = startNum; o > endNum; o--) {
+      totalNum += o;
+    }
+  }
+  return totalNum;
 };
 
 // Do not edit below this line
