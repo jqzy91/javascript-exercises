d92d8e098d8ff654d071ecb08dbe5e62e9d2f155
jqzy91 <jamesquek91@live.com.sg>
2022-03-30 18:12:02 +0800

    exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function (firstAdd, secondAdd) {
+  result = firstAdd + secondAdd;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (firstSub, secondSub) {
+  result = firstSub - secondSub;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (sumArray) {
+  let result = 0;
+  for (let i = 0; i < sumArray.length; i++) {
+    result += sumArray[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function (multiArray) {
+  let result = 1;
+  for (let i = 0; i < multiArray.length; i++) {
+    result *= multiArray[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function (num, powerOf) {
+  let result = num;
+  for (let i = 1; i < powerOf; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  for (let i = num; 0 < i; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +47,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
