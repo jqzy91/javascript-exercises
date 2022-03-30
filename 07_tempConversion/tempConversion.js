f443ed2e267528f38da797efc18e555f0d7bbafc
jqzy91 <jamesquek91@live.com.sg>
2022-03-30 10:08:31 +0800

    07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+  let celsius = (f - 32) * (5 / 9);
+  return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+  let fahrenheit = c * (9 / 5) + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
