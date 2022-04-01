a07384987b5b94714093226ebb82472e11be0740
jqzy91 <jamesquek91@live.com.sg>
2022-04-01 16:16:13 +0800

    exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  let inputFormat = input.toLowerCase();
+  let palindrome = inputFormat.replace(/,|\.|!|\s/g, "");
+  let o = palindrome.length - 1;
+  checkPalindrome: for (let i = 0; i < palindrome.length; i++) {
+    if (palindrome[i] === palindrome[o]) {
+      o--;
+      continue checkPalindrome;
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+//required: remove commas, fullstops, exclamations and spaces. Lowercase conversion.
+
+//for(let i = 0; i < result.length; i++) {
+//  if (result[i] !== `\w`) {
+
+//  }
+// }
