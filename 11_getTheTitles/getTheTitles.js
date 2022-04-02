9cf9419a929502ab9942e1d0e747ded7cb5b2064
jqzy91 <jamesquek91@live.com.sg>
2022-04-02 22:06:40 +0800

    exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,22 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function () {
+  const result = [];
+  console.log(books.length);
+  for (let i = 0; i < books.length; i++) {
+    result.push(books[i]["title"]);
+    continue;
+  }
+  return result;
 };
 
 // Do not edit below this line
